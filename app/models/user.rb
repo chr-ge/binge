9162e08c921c214fc06ceefea9f5221d284a31db
chr-ge <ace26@protonmail.com>
2020-09-16 19:00:08 -0400

    Added User CRUD

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6, allow_blank: true }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
+end
