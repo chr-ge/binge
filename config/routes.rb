8976de6100f6dd151d561739388a8d67b235b84c
chr-ge <ace26@protonmail.com>
2020-09-17 01:13:36 -0400

    Created Likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :favorites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
 
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
 
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 end
