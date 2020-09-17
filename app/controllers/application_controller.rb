e10a3917b7fed4c0471a73e8245c3195c4e26498
chr-ge <ace26@protonmail.com>
2020-09-17 12:48:00 -0400

    Changed required signin url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
         def require_signin
             unless current_user
                 session[:intended_url] = request.url
-                redirect_to new_session_url, alert: "SignIn Required."
+                redirect_to signin_url, alert: "SignIn Required."
             end
         end
 
