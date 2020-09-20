7dc2817c08a93e4eca6d8af847e7a88600063b7b
chr-ge <ace26@protonmail.com>
2020-09-20 14:18:43 -0400

    Created Movie Model & Controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  setup do
+    @movie = movies(:spiderman2)
+  end
+
+  test "should get movies index" do
+    get movies_url
+    assert_response :success
+  end
+
+  # test "should get movies new" do
+  #   get new_movie_url
+  #   assert_response :success
   # end
+
+  # test "should create movie" do
+  #   assert_difference('Movie.count') do
+  #     post movies_url, params: { movie: { 
+  #       title: @movie.title, rating: @movie.rating, total_gross: @movie.total_gross, 
+  #       duration: @movie.duration, released_on: @movie.released_on, description: @movie.description, director: @movie.director
+  #     }}
+  #   end
+
+  #   assert_redirected_to movie_url(@movie)
+  # end
+
+  test "should get movie" do
+    get movie_url(@movie)
+    assert_response :success
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "create movie with valid attributes" do
+    movie = Movie.new(title: "Captain Marvel 2", rating: "PG", total_gross: 654654654.0, 
+      director: "Good Director", description: "Captain Marvel 2 Movie PG 1999", released_on: "1999-12-15 00:00:00", 
+      duration: "181 min")
+    assert movie.save
+  end
+
+  test "create movie with valid rating" do
+    movie = Movie.new(title: "Captain Marvel 2", rating: "PG-99", total_gross: 654654654.0, 
+      director: "Good Director", description: "Captain Marvel 2 Movie PG 1999", released_on: "1999-12-15 00:00:00", 
+      duration: "181 min")
+    assert_not movie.save
+  end
 end
